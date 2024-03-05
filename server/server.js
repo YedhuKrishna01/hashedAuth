aa67b27ad3f528cad4af8eba6aa400cd62c509a5
YedhuKrishna01 <yedhukrishnagk@gmail.com>
2024-03-05 22:24:11 +0530

    backend updated

--- server/server.js
+++ server/server.js
@@ -1,1 +1,9 @@
 const express = require('express');
+const dotenv = require('dotenv').config
+const cors = require('cors');
+
+const app = express()
+
+const port = 3000;
+
+app.listen(port,()=> console.log(`Server is running on port ${port}`))
